fe68ab231ddcd6fe3dac09595e3745a2d59c3dd2
unknown <deen.aliyu40@gmail.com>
2022-10-24 13:27:07 +0100

    first commit

---
+++ NG 2019/mapdata.js
@@ -0,0 +1,344 @@
+var simplemaps_countrymap_mapdata={
+  main_settings: {
+    //General settings
+		width: "responsive", //or 'responsive'
+    background_color: "#FFFFFF",
+    background_transparent: "yes",
+    popups: "detect",
+    
+		//State defaults
+		state_description: "State description",
+    state_color: "#88A4BC",
+    state_hover_color: "#3B729F",
+    state_url: "NG 2019/demo.html",
+    border_size: 1.5,
+    border_color: "#ffffff",
+    all_states_inactive: "no",
+    all_states_zoomable: "no",
+    
+		//Location defaults
+		location_description: "Location description",
+    location_color: "#FF0067",
+    location_opacity: 0.8,
+    location_hover_opacity: 1,
+    location_url: "",
+    location_size: 25,
+    location_type: "square",
+    location_border_color: "#FFFFFF",
+    location_border: 2,
+    location_hover_border: 2.5,
+    all_locations_inactive: "no",
+    all_locations_hidden: "no",
+    
+		//Label defaults
+		label_color: "#ffffff",
+    label_hover_color: "#ffffff",
+    label_size: 22,
+    label_font: "Arial",
+    hide_labels: "no",
+    hide_eastern_labels: false,
+   
+		//Zoom settings
+		manual_zoom: "yes",
+    back_image: "no",
+    arrow_box: "no",
+    navigation_size: "40",
+    navigation_color: "#f7f7f7",
+    navigation_border_color: "#636363",
+    initial_back: "no",
+    initial_zoom: -1,
+    initial_zoom_solo: "no",
+    region_opacity: 1,
+    region_hover_opacity: 0.6,
+    zoom_out_incrementally: "yes",
+    zoom_percentage: 0.99,
+    zoom_time: 0.5,
+    
+		//Popup settings
+		popup_color: "white",
+    popup_opacity: 0.9,
+    popup_shadow: 1,
+    popup_corners: 5,
+    popup_font: "14px/1.5 Verdana, Arial, Helvetica, sans-serif",
+    popup_nocss: "no",
+    
+		//Advanced settings
+		div: "map",
+    auto_load: "yes",
+    rotate: "0",
+    url_new_tab: "yes",
+    images_directory: "default",
+    import_labels: "no",
+    fade_time: 0.1,
+    link_text: "View Website"
+  },
+  state_specific: {
+    NGA2839: {
+      name: "Borno",
+      description: "TOTAL NO OF LGA WON = 24 TOTAL NVR = 2,315,956  TOTAL NO OF VOTES CAST= 919,786",
+      color: "#34BCE4",
+      hover_color: "YELLOW",
+      url: "default"
+    },
+    NGA2841: {
+      name: "Abia",
+      description: "TOTAL NO OF LGA WON = 12 OUT OF 17 TOTAL NVR = 1,932,892 TOTAL NO OF ACCREDITED VOTERS= 361,561 TOTAL NO OF VOTES CAST= 323,291",
+      color: "RED",
+      hover_color: "MAROON",
+      url: "default"
+    },
+    NGA2842: {
+      name: "Akwa Ibom",
+      description: "TOTAL NO OF LGA WON = 14 OUT OF 31 TOTAL NVR =2,119,727 TOTAL NO OF VOTES CAST= 578,775",
+      color: "RED",
+      hover_color: "MAROON",
+      url: "default"
+    },
+    NGA2843: {
+      name: "Imo",
+      description: "TOTAL NO OF LGA WON = 24 OUT OF 27 TOTAL NVR = 2,272,293 TOTAL NO OF ACCREDITED VOTERS= 585,741 TOTAL NO OF VOTES CAST= 511,586",
+      color: "RED",
+      hover_color: "MAROON",
+      url: "default"
+    },
+    NGA2844: {
+      name: "Rivers",
+      description: "TOTAL NO OF LGA WON = 16 OUT OF 23 TOTAL NVR = 3,215,273 TOTAL NO OF VOTES CAST= 642,165",
+      color: "RED",
+      hover_color: "MAROON",
+      url: "default"
+    },
+    NGA2845: {
+      name: "Bayelsa",
+      description: "TOTAL NO OF LGA WON = 4 OUT OF 8 TOTAL NVR = 923,182 TOTAL NO OF VOTES CAST= 321,767 ",
+      color: "RED",
+      hover_color: "MAROON",
+      url: "default"
+    },
+    NGA2846: {
+      name: "Benue",
+      description: "TOTAL NO OF LGA WON = 16 OUT OF 23 TOTAL NVR =2,480,131 TOTAL NO OF ACCREDITED VOTERS=786,069 TOTAL NO OF VOTES CAST= 728,912",
+      color: "RED",
+      hover_color: "MAROON",
+      url: "default"
+    },
+    NGA2847: {
+      name: "Cross River",
+      description: "TOTAL NO OF LGA WON = 12 OUT OF 18 TOTAL NVR = 1,527,289 TOTAL NO OF VOTES CAST= 421,901",
+      color: "RED",
+      hover_color: "MAROON",
+      url: "default"
+    },
+    NGA2848: {
+      name: "Taraba",
+      description: "TOTAL NO OF LGA WON = 8 OUT OF 16 TOTAL NVR = 1,777,105 TOTAL NO OF ACCREDITED VOTERS= 756,111 TOTAL NO OF VOTES CAST= 712,877",
+      color: "RED",
+      hover_color: "MAROON",
+      url: "default"
+    },
+    NGA2849: {
+      name: "Kwara",
+      description: "TOTAL NO OF LGA WON = 12 OUT OF 16 TOTAL NVR = 1,406,457 TOTAL NO OF ACCREDITED VOTERS= 489,482 TOTAL NO OF VOTES CAST= 459,676",
+      color: "#34BCE4",
+      hover_color: "YELLOW",
+      url: "default"
+    },
+    NGA2850: {
+      name: "Lagos",
+      description: "TOTAL NO OF LGA WON = 15 OUT OF 20 TOTAL NVR = 6,570,291 TOTAL NO OF ACCREDITED VOTERS= 1,196,490 TOTAL NO OF VOTES CAST= 1,089,567",
+      color: "#34BCE4",
+      hover_color: "YELLOW",
+      url: "default"
+    },
+    NGA2851: {
+      name: "Niger",
+      description: "TOTAL NO OF LGA WON = 21 OUT OF 25 TOTAL NVR = 2,390,035 TOTAL NO OF ACCREDITED VOTERS= 911,964 TOTAL NO OF VOTES CAST= 851,937",
+      color: "#34BCE4",
+      hover_color: "YELLOW",
+      url: "default"
+    },
+    NGA2852: {
+      name: "Ogun",
+      description: "TOTAL NO OF LGA WON = 14 OUT OF 20 TOTAL NVR = 2,375,003 TOTAL NO OF ACCREDITED VOTERS= 613,397 TOTAL NO OF VOTES CAST= 564,256",
+      color: "#34BCE4",
+      hover_color: "YELLOW",
+      url: "default"
+    },
+    NGA2853: {
+      name: "Ondo",
+      description: "TOTAL NO OF LGA WON = 11 OUT OF 18 TOTAL NVR = 1,822,346 TOTAL NO OF ACCREDITED VOTERS= 598,586 TOTAL NO OF VOTES CAST= 555,994",
+      color: "RED",
+      hover_color: "MAROON",
+      url: "default"
+    },
+    NGA2854: {
+      name: "Ekiti",
+      description: "TOTAL NO OF LGA WON = 15 OUT OF 16 TOTAL NVR =909,967 TOTAL NO OF ACCREDITED VOTERS= 395,741 TOTAL NO OF VOTES CAST= 381,132",
+      color: "#34BCE4",
+      hover_color: "YELLOW",
+      url: "default"
+    },
+    NGA2855: {
+      name: "Osun",
+      description: "TOTAL NO OF LGA WON = 18 OUT OF 30 TOTAL NVR = 1,680,498 TOTAL NO OF ACCREDITED VOTERS= 570,773 TOTAL NO OF VOTES CAST= 714,682",
+      color: "#34BCE4",
+      hover_color: "YELLOW",
+      url: "default"
+    },
+    NGA2856: {
+      name: "Oyo",
+      description: "TOTAL NO OF LGA WON = 18 OUT OF 33 TOTAL NVR = 2,934,107 TOTAL NO OF ACCREDITED VOTERS=905,007 TOTAL NO OF VOTES CAST= 836,531 ",
+      color: "RED",
+      hover_color: "MAROON",
+      url: "default"
+    },
+    NGA2857: {
+      name: "Anambra",
+      description: "TOTAL NO OF LGA WON = 21 OUT OF 21 TOTAL NVR = 2,466,638 TOTAL NO OF ACCREDITED VOTERS= 675,273 TOTAL NO OF VOTES CAST= 605,734",
+      color: "RED",
+      hover_color: "MAROON",
+      url: "default"
+    },
+    NGA2858: {
+      name: "Bauchi",
+      description: "TOTAL NO OF LGA WON = 15 TOTAL NVR = 2,462,843 TOTAL NO OF ACCREDITED VOTERS= 1,075,330 TOTAL NO OF VOTES CAST=1,024,307",
+      color: "#34BCE4",
+      hover_color: "YELLOW",
+      url: "default"
+    },
+    NGA2859: {
+      name: "Gombe",
+      description: "TOTAL NO OF LGA WON = 8 OUT OF TOTAL NVR =1,394,393 TOTAL NO OF ACCREDITED VOTERS=467,784 TOTAL NO OF VOTES CAST= 554,203",
+      color: "#34BCE4",
+      hover_color: "YELLOW",
+      url: "default"
+    },
+    NGA2860: {
+      name: "Delta",
+      description: "TOTAL NO OF LGA WON = 17 OUT OF 25 TOTAL NVR = 2,845,274 TOTAL NO OF VOTES CAST= 829,762",
+      color: "RED",
+      hover_color: "MAROON",
+      url: "default"
+    },
+    NGA2861: {
+      name: "Edo",
+      description: "TOTAL NO OF LGA WON = 9 OUT OF 18 TOTAL NVR = 2,210,534 TOTAL NO OF ACCREDITED VOTERS= 604,915 TOTAL NO OF VOTES CAST= 560,711",
+      color: "RED",
+      hover_color: "MAROON",
+      url: "default"
+    },
+    NGA2862: {
+      name: "Enugu",
+      description: "TOTAL NO OF LGA WON = 17 OUT OF 17 TOTAL NVR = 1,944,016 TOTAL NO OF ACCREDITED VOTERS= 452,765 TOTAL NO OF VOTES CAST= 421,014",
+      color: "RED",
+      hover_color: "MAROON",
+      url: "default"
+    },
+    NGA2863: {
+      name: "Ebonyi",
+      description: "TOTAL NO OF LGA WON = 12 OUT OF 13 TOTAL NVR =1,459,933 TOTAL NO OF ACCREDITED VOTERS= 391,747 TOTAL NO OF VOTES CAST= 359,131",
+      color: "RED",
+      hover_color: "MAROON",
+      url: "default"
+    },
+    NGA2864: {
+      name: "Kaduna",
+      description: "TOTAL NO OF LGA WON = 15 OUT OF 23 TOTAL NVR = 3,932,492 TOTAL NO OF ACCREDITED VOTERS= 1,757,868 TOTAL NO OF VOTES CAST= 1,663,603",
+      color: "#34BCE4",
+      hover_color: "YELLOW",
+      url: "default"
+    },
+    NGA2865: {
+      name: "Kogi",
+      description: "TOTAL NO OF LGA WON = 16 OUT OF 21 TOTAL NVR = 1,646,350 TOTAL NO OF ACCREDITED VOTERS= 570,773 TOTAL NO OF VOTES CAST= 521,016",
+      color: "#34BCE4",
+      hover_color: "YELLOW",
+      url: "default"
+    },
+    NGA2866: {
+      name: "Plateau",
+      description: "TOTAL NO OF LGA WON = 12 OUT OF 16 TOTAL NVR =2,480,455 TOTAL NO OF ACCREDITED VOTERS= 1,074,042 TOTAL NO OF VOTES CAST=",
+      color: "RED",
+      hover_color: "MAROON",
+      url: "default"
+    },
+    NGA2867: {
+      name: "Nassarawa",
+      description: "TOTAL NO OF LGA WON = 7 OUT OF 13 TOTAL NVR = 1,617,786 TOTAL NO OF ACCREDITED VOTERS= 613,720 TOTAL NO OF VOTES CAST=",
+      color: "#34BCE4",
+      hover_color: "YELLOW",
+      url: "default"
+    },
+    NGA2868: {
+      name: "Jigawa",
+      description: "TOTAL NO OF LGA WON = 27 OUT OF 27 TOTAL NVR = 2,111,106 TOTAL NO OF ACCREDITED VOTERS= 1,171,801 TOTAL NO OF VOTES CAST=",
+      color: "#34BCE4",
+      hover_color: "YELLOW",
+      url: "default"
+    },
+    NGA2869: {
+      name: "Kano",
+      description: "TOTAL NO OF LGA WON = 32 OUT OF 44 TOTAL NVR = 5,457,747 TOTAL NO OF ACCREDITED VOTERS= 2,006,410 TOTAL NO OF VOTES CAST=",
+      color: "#34BCE4",
+      hover_color: "YELLOW",
+      url: "https://atlantalegalattorney.com/"
+    },
+    NGA2870: {
+      name: "Katsina",
+      description: "TOTAL NO OF LGA WON = 34 OUT OF 34 TOTAL NVR = 3,230,230 TOTAL NO OF ACCREDITED VOTERS= 1,628,865 TOTAL NO OF VOTES CAST=",
+      color: "#34BCE4",
+      hover_color: "YELLOW",
+      url: "default"
+    },
+    NGA2871: {
+      name: "Sokoto",
+      description: "TOTAL NO OF LGA WON = 17 OUT OF 23 TOTAL NVR =1,903,166 TOTAL NO OF VOTES CAST=",
+      color: "#34BCE4",
+      hover_color: "YELLOW",
+      url: "default"
+    },
+    NGA2872: {
+      name: "Zamfara",
+      description: "TOTAL NO OF LGA WON = 14 TOTAL NVR =1,717,128 TOTAL NO OF ACCREDITED VOTERS= NIL TOTAL NO OF VOTES CAST=",
+      color: "#34BCE4",
+      hover_color: "YELLOW",
+      url: "default"
+    },
+    NGA2873: {
+      name: "Yobe",
+      description: "TOTAL NO OF LGA WON = 15 OUT OF 17 TOTAL NVR =1,365,913 TOTAL NO OF ACCREDITED VOTERS= 601,059 TOTAL NO OF VOTES CAST=",
+      color: "#34BCE4",
+      hover_color: "YELLOW",
+      url: "default"
+    },
+    NGA2879: {
+      name: "Kebbi",
+      description: "TOTAL NO OF LGA WON = 21 OUT OF 21 TOTAL NVR = 1,806,231 TOTAL NO OF ACCREDITED VOTERS= NIL TOTAL NO OF VOTES CAST=",
+      color: "#34BCE4",
+      hover_color: "YELLOW",
+      url: "default"
+    },
+    NGA2881: {
+      name: "Adamawa",
+      description: "TOTAL NO OF LGA WON = 10 TOTAL NVR = 1,973,083 TOTAL NO OF ACCREDITED VOTERS= 874,920 TOTAL NO OF VOTES CAST=",
+      color: "RED",
+      hover_color: "MAROON",
+      url: "default"
+    },
+    NGA3470: {
+      name: "Federal Capital Territory",
+      description: "TOTAL NO OF LGA WON = 5 OUT OF TOTAL NVR = 1,344,856 TOTAL NO OF ACCREDITED VOTERS= 467,784 TOTAL NO OF VOTES CAST=",
+      color: "RED",
+      hover_color: "MAROON",
+      url: "default"
+    }
+  },
+  locations: {
+    "0": {
+      lat: "9.083333",
+      lng: "7.533333",
+      name: "Abuja"
+    }
+  },
+  labels: {}
+};
